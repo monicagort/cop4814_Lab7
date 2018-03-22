f49bf1eee67c49d45c40c0862ac72565f90edcbc
Monica Gort <37599832+monicagort@users.noreply.github.com>
2018-03-22 17:37:57 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    public class Game
+    {
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Team1Score { get; set; }
+        public int Team2Score { get; set; }
+
+        public Game() { }
+
+        public Game ( string Team1, string Team2, int Team1Score, int Team2Score )
+        {
+            this.Team1 = Team1;
+            this.Team2 = Team2;
+            this.Team1Score = Team1Score;
+            this.Team2Score = Team2Score;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}{2}{3} {4} {5} {6}{7}{8}", Team1, "(", Team1Score, ")", "-", Team2, "(", Team2Score, ")");
+        }
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_2;
+
+namespace GameFactory
+{
+    public class GameFactory
+    {
+        List<Game> gameList = new List<Game>();
+
+        public void CreateGameList()
+        {
+            Game games;
+            games = new Game("Celtics", "Rockets", 78, 90);
+            gameList.Add(games);
+
+            games = new Game("Red Sox", "Yankees", 5, 9);
+            gameList.Add(games);
+
+            games = new Game("Patriots", "Seahawks", 21, 28);
+            gameList.Add(games);
+
+            games = new Game("Dolphins", "Raiders", 49, 0);
+            gameList.Add(games);
+
+            games = new Game("Hurricanes", "Panters", 36, 70);
+            gameList.Add(games);
+
+            games = new Game("Jaguars", "Vikings", 14, 9);
+            gameList.Add(games);
+        }
+    }
+}
